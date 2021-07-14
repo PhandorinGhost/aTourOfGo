dee3517699d7c90b733df1a961cdb3f78c386cd2
Roman Phandorin <roman.phandorin@gmail.com>
2021-07-14 10:47:24 +0300

    Exercise 'rot13Reader' is ready

--- main.go
+++ main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-	"golang.org/x/tour/reader"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"unicode"
 )
 
-type MyReader struct{}
-
-func (r MyReader) Read(b []byte) (n int, err error) {
-	for i := 0; i < len(b); i++ {
-		b[i] = 'A'
+func rot13(bt byte) byte {
+	if !unicode.IsLetter(rune(bt)) {
+		return bt
+	}
+	var rez byte
+	if (bt >= 'a' && bt <= 'm') || (bt >= 'A' && bt <= 'M') {
+		rez = bt + 13
+	} else if (bt >= 'n' && bt <= 'z') || (bt >= 'N' && bt <= 'Z') {
+		rez = bt - 13
 	}
 
-	return len(b), nil
+	return rez
+}
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot rot13Reader) Read(b []byte) (int, error) {
+	number, err := rot.r.Read(b)
+	if err != nil {
+		return 0, err
+	}
+
+	for i := 0; i < number; i++ {
+		b[i] = rot13(b[i])
+	}
+
+	return number, err
 }
 
 func main() {
-	reader.Validate(MyReader{})
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	_, err := io.Copy(os.Stdout, &r)
+	if err != nil {
+		log.Println(err)
+	}
 }
